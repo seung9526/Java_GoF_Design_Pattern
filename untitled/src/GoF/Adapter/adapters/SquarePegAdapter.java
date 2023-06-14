8d9db5c698eb7f01904cbbcb341bd5e9c77db042
seung9526 <95rus@naver.com>
2023-06-14 09:31:29 +0900

    어댑터(Adapter) 패턴

---
+++ untitled/src/GoF/Adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,19 @@
+package GoF.Adapter.adapters;
+
+import GoF.Adapter.round.RoundPeg;
+import GoF.Adapter.square.SquarePeg;
+
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg){
+        this.peg =peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        result = (Math.sqrt(Math.pow((peg.getWidth()/2),2)*2));
+        return result;
+    }
+}
